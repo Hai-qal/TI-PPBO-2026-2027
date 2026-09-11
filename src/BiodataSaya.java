c5df4d3f228f79ab31701c076217a5690fdd4870
Haiqal <haiqalini145@gmail.com>
2026-09-12 01:22:20 +0700

    Latihan.2. Biodata

--- src/BiodataSaya.java
+++ src/BiodataSaya.java
@@ -7,5 +7,16 @@ public class BiodataSaya {
         // Menampilkan NIM serta prodi
         System.out.println("NIM: 2025573010035");
         System.out.println("Program Studi: Teknik Informatika");
+
+        // Ditampilkan agar bisa melihat perbedaan outputnya
+        System.out.println("=================================");
+
+        // Menampilkan nama dan NIM dalam satu baris
+        System.out.print("Nama: Haiqal");
+        System.out.print(" - NIM: 2025573010035");
+
+        //Pindah ke baris selanjutnya
+        System.out.println();
+        System.out.println("Program Studi: Teknik Informatika");
     }
 }
