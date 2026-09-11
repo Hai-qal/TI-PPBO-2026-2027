fdbe553b63d9f6d489bd8b1b8e04445f6486f1f6
Haiqal <haiqalini145@gmail.com>
2026-09-12 00:16:53 +0700

    A.3. Komentar Kode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,11 @@ public class HelloWorld {
         System.out.print("Budi");
         System.out.println();
         System.out.println("Ini baris baru");
+
+        // Program menampilkan biodata sederhana
+        System.out.println("Nama: Budi");
+        /* Baris di bawah ini mencetak
+           NIM mahasiswa */
+        System.out.println("NIM: 12345");
     }
 }
